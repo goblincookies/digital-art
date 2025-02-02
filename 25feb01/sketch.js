32b498032f204177708eff8b2fd7015bd37893e1
goblincookies <goblincookiebox@gmail.com>
2025-02-02 15:04:06 -0500

    random points

--- 25feb01/sketch.js
+++ 25feb01/sketch.js
@@ -1,9 +1,21 @@
-function setup() {
+let points = [];
 
+function setup() {
   const myCanvas = createCanvas(800, 800);
   myCanvas.parent("box");
+
+  for(let i=0; i <100; i++) {
+    points[i] = createVector(random(width), random(height));
+  }
+
 }
 
 function draw() {
-  background(220);
+  background(245);
+
+  for (let v of points){
+    stroke(0);
+    strokeWeight(4);
+    point(v.x, v.y);
+  }
 }
